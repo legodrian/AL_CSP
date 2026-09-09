1b900ac73565240f8a8c4fbaa0406d40d1198f24
legodrian <adrian.leon@ucas-edu.net>
2026-09-09 14:51:50 -0600

    Variables Notes

---
+++ variables_notes.py
@@ -0,0 +1,8 @@
+# AL, Variables Notes
+
+name = input("Tell me your name: ")
+
+
+print("CPS is the best class ever !!!!")
+print("Lasagna besto food")
+print("Hello",name, "Welcome to the best class")
