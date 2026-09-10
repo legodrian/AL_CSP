535c9a47756b7b21d0dfd546398a2d8d837317e6
legodrian <adrian.leon@ucas-edu.net>
2026-09-10 14:34:09 -0600

    Variables Practice

---
+++ variables_practice.py
@@ -0,0 +1,24 @@
+# AL, Variables Practice
+
+name = "Adrian"
+number = "8"
+number_2 = "216"
+breakfast = "eggs"
+fav_color = "blue"
+school = "Timpanogos"
+year = "2026"
+eye_color = "brown" 
+age = "75"
+fav_school_subject = "computer_science_principles_:)"
+
+print(name)
+print(number)
+print(number_2)
+print(breakfast)
+print(fav_color)
+print(school)
+print(year)
+print(eye_color)
+print(age)
+print(fav_school_subject)
+
