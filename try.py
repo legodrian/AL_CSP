f6a61752f4f4f35912179492f9624b463a36b9db
legodrian <adrian.leon@ucas-edu.net>
2026-09-15 14:49:30 -0600

    outschool work

---
+++ try.py
@@ -0,0 +1,12 @@
+#outschool work
+
+while True:
+    number = input("Tell me your favorite number: ").strip()
+    if number.isnumeric():
+
+        print(f"{number} is a cool number!")
+
+    elif " " in number: 
+       print("I said number.")
+    else:
+       break
