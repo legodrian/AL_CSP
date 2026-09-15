8afc545a025ef2b9bd188795898ab7709c1620b9
legodrian <adrian.leon@ucas-edu.net>
2026-09-15 14:37:34 -0600

    9/15

---
+++ outschool_work.py
@@ -0,0 +1,12 @@
+#Out school silly tries 
+
+variable = input ("money: ")
+
+purchase = "you bought a carrot, yay!"
+not_purchase = "you dont have enough money, poor"
+
+if variable >= 100 : 
+    print(purchase)
+else: 
+    
+    print(not_purchase)  
