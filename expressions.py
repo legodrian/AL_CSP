50c2a300e8847e93e1e8dc714a62968b71ce2179
legodrian <adrian.leon@ucas-edu.net>
2026-09-17 14:47:08 -0600

    9/17

--- expressions.py
+++ expressions.py
@@ -14,7 +14,7 @@ rain = 2.17
 
 #Arithmetic operators (+ - * / // ** %)
 print(f"18/4 is {18/4} or {18//4} with a remainder of {18%4}")
-print(f"18/4 is {18/5} or {18//5} with a remainder of {18%5}")\
+print(f"18/4 is {18/5} or {18//5} with a remainder of {18%5}")
 
 #Orders of operation 
 grades = [85,66,94,72,88,79,100]
