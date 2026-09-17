3b4bc2f037837e93c1b27e122ab33df9d5664824
legodrian <adrian.leon@ucas-edu.net>
2026-09-17 14:50:33 -0600

    9/17 #2

--- your_budget.py
+++ your_budget.py
@@ -11,15 +11,16 @@ utilities_1 = int(round((utilities / income) * 100))
 groceries_1 = int(round((groceries / income) * 100)) 
 transportation_1 = int(round((transportation / income) * 100)) 
 
-spending_money = income - rent - utilities - groceries - transportation
+savings = income * 0.10
+savings_1 = 10 
+
+
+spending_money = income - rent - utilities - groceries - transportation - savings
 
 print(f"You rent is ${rent} and is {rent_1} percent of your income")
 print(f"You utilities are ${utilities} and is {utilities_1} percent of your income")
 print(f"You groceries are ${groceries} and is {groceries_1} percent of your income")
 print(f"You transportation is ${transportation} and is {transportation_1} percent of your income")
-
-savings = income * 0.10
-savings_1 = 10 
 
 print(f"You should save ${savings} and is {savings_1} percent of your income")
 print(f"You have ${spending_money} of spending money each month")
