9cb113c7967cede3cd4fa663def6754c81473b4a
legodrian <adrian.leon@ucas-edu.net>
2026-09-24 14:04:13 -0600

    9/24 #1

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,36 +1,77 @@
 # Al, Password strength Checker
 
-#conditional to password long enough 
-#update length variable to "true"
-
-#for letter in password: 
-
 characters = False 
 uppercase_letter = False
 lowercase_letter = False
 number = False
 symbol = False
 
+
 password = input("What is your password: ")
 
-for letter in password :
-    if len(password) < 8 : 
-     characters = True 
+
+if len(password) >= 8: 
+    characters = True 
 
 
-for uppercarse_letter in password :
-   if password.isupper() : 
-    uppercase_letter = True 
+for letter in password:
+    if letter.isupper(): 
+        uppercase_letter = True  
+    elif letter.islower(): 
+        lowercase_letter = True  
+    elif letter.isdigit(): 
+        number = True  
+    else: 
+        symbol = True
 
-for lower_case in password : 
-   if password.islower() : 
-    lowercase_letter = True 
 
-for number in password : 
-  if password.isnumeric() : 
-    number = True 
+strength = 0
 
-for symbol in password : 
-  if letter in "!@#$%?/" : 
-    symbol = True 
+if characters == True:
+    strength += 1
 
+if uppercase_letter == True:
+    strength += 1
+
+if lowercase_letter == True:
+    strength += 1
+
+if number == True:
+    strength += 1
+
+if symbol == True:
+    strength += 1
+
+password_type = 0
+
+if strength == 5:
+    password_type = "Strong"
+elif strength == 3 or strength == 4:
+    password_type = "Medium"
+else:
+    password_type = "Weak"
+
+
+print(f"At least 8 characters: {characters}")
+print(f"Has an uppercase letter: {uppercase_letter}")
+print(f"Has a lowercase letter: {lowercase_letter}")
+print(f"Has a number: {number}")
+print(f"Has a symbol: {symbol}")
+print(f"Your password strength is: {password_type}")
+
+
+if password_type != "Strong":
+    missing = ""
+    
+    if characters == False:
+        missing += "at least 8 characters, "
+    if uppercase_letter == False:
+        missing += "an uppercase letter, "
+    if lowercase_letter == False:
+        missing += "a lowercase letter, "
+    if number == False:
+        missing += "a number, "
+    if symbol == False:
+        missing += "a symbol, "
+        
+    print(f"To make it Strong, add: {missing}")
